89e9e838b31aeb79d607c662a8687556d5b4f060
AxiomaPartnersDev <axiomapartnersdev@gmail.com>
2023-02-02 10:57:10 +0200

    add dbconnect

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 export default {
     port: Number(process.env.PORT) || 4010,
-    dbConnectLink: process.env.DBCONNECTLINK || '',
+    dbConnectLink: process.env.DBCONNECTLINK || 'mongodb://localhost:27017/ekki',
 }
 
